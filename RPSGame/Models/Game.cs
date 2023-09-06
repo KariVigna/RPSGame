5bfe194d64ef5f99acea323e7a853d9896d85a13
Kari Vigna <karivigna24@gmail.com>
2023-09-05 19:01:41 -0700

    Co-authored-by: brian yoder <brnydr@users.noreply.github.com> Co-authored-by: Julien <julienfullstack@users.noreply.github.com> Co-authored-by: ElenaRosa <elenarosamail@gmail.com>

---
+++ RPSGame/Models/Game.cs
@@ -0,0 +1,7 @@
+namespace ProjectName.Models
+{
+  public class ClassName
+  {
+    // Business Logic goes here.
+  }
+}
---
+++ RPSGame/Program.cs
@@ -0,0 +1,10 @@
+namespace ProjectName
+{
+  class Program
+  {
+    static void Main()
+    {
+      // UI code goes here.
+    }
+  }
+}
